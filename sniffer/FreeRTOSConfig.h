80912bbc93a565d5375e697612ad9aea0990a7dc
Bruno Van de Velde <vdv_b@tgui.eu>
2015-12-11 16:02:47 +0100

    Steal some memory reserved for the heap to increase buffer size
    The heap size can be further reduced to 2048 bytes, but since
    the program didn't work with a heap size of 1536 this might be too close
    to the limit. So 3072 was chosen to not introduce sudden problems
    during further development.

--- sniffer/FreeRTOSConfig.h
+++ sniffer/FreeRTOSConfig.h
@@ -24,7 +24,7 @@
 #define configUSE_TICK_HOOK				        0
 #define configMAX_PRIORITIES			        ( 5 )
 #define configMINIMAL_STACK_SIZE		        ( ( unsigned short ) 64 )
-#define configTOTAL_HEAP_SIZE			        ( ( size_t ) ( 4 * 1024 ) )
+#define configTOTAL_HEAP_SIZE			        ( ( size_t ) ( 3 * 1024 ) )
 #define configMAX_TASK_NAME_LEN			        ( 16 )
 #define configUSE_TRACE_FACILITY		        0
 #define configUSE_16_BIT_TICKS			        0
